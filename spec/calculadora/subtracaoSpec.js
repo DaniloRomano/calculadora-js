06cb32f25982beb51182bdd3c63cc5253cb6fd4b
danilo.romano <danilo.romano@azi.com.br>
2021-07-16 15:30:29 -0400

    Primeiro suite de Testes - Função subtração

---
+++ spec/calculadora/subtracaoSpec.js
@@ -0,0 +1,27 @@
+describe("Operação de subtração: ", function () {
+    var Calculadora = require('../../src/js/calculadora');
+
+    it("Deve retornar 5 para os valores 2 e 3: ",
+        function () {
+            expect(Calculadora.subtrair(2, 3)).toEqual(-1);
+        });
+
+    it("Deve retornar 6 para os valores 9 e -3 em formato texto",
+        function () {
+            expect(Calculadora.subtrair('9', '-3')).toEqual(12);
+        });
+
+    it("Deve retornar 6.5 para os valores 1.5 e 5", function () {
+        expect(Calculadora.subtrair(1.5, 5)).toEqual(-3.5);
+    });
+
+    it("Deve retornar 0 quando o primeiro valor for inválido", function () {
+        expect(Calculadora.subtrair(undefined, 10)).toEqual(0);
+    });
+
+    it("Deve retornar 0 quando o segundo valor for inválido", function () {
+        expect(Calculadora.subtrair(10, undefined)).toEqual(0);
+    });
+
+
+});
--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -4,7 +4,12 @@ var Calculadora = {
         return parseFloat(num1) + parseFloat(num2);
     },
     subtrair: (num1, num2) => {
-        return 0;
+        var result = 0;
+        if (isNaN(num1) || isNaN(num2)) return 0;
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        result = num1 - num2;
+        return result;
     },
     multiplicar: (num1, num2) => {
         return 0;
